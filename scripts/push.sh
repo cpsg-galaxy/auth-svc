5da9a03162b9be559ac482b1cdebdfffa345cb2f
Ali Ghorbani Moghadam <amoghada@cisco.com>
2019-10-22 19:21:44 -0400

    Adding push to AWS for push.sh

--- scripts/push.sh
+++ scripts/push.sh
@@ -2,4 +2,4 @@
 
 docker build --build-arg JAR_FILE=build/libs/auth-svc.jar -t auth-svc:$BUILD_NUMBER.$BUILD_ID .
 echo $DOCKER_PSWRD | docker login --username $DOCKER_USER --password-stdin $DOCKER_REGISTRY
-
+docker push $DOCKER_REGISTRY/auth-svc:$BUILD_NUMBER.$BUILD_ID
