8603962f3b7db998297e8b402a4a530790113fa2
Ali Ghorbani Moghadam <amoghada@cisco.com>
2019-10-22 16:57:13 -0400

    updating push.sh

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo $FOO
+docker login -u $DOCKER_USER -p $DOCKER_PSWRD $DOCKER_REGISTRY
+docker build --build-arg JAR_FILE=build/libs/auth-svc.jar -t auth-svc:$BUILD_NUMBER.$BUILD_ID .
+docker push $DOCKER_REGISTRY/auth-svc:$BUILD_NUMBER.$BUILD_ID
