cdb5eb557508b7d4e0d4fa19dc0746949da26108
Ali Ghorbani Moghadam <amoghada@cisco.com>
2019-10-22 10:04:03 -0400

    Updating Jenkinsfile

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rm -rf build
+rm -rf "$PWD":/home/gradle/project/build
 docker run --rm -v "$PWD":/home/gradle/project -w /home/gradle/project gradle gradle clean bootJar -x test -x jacocoTestCoverageVerification
