bcb45f5ba5a791441a429425ebaf58602cc9a11a
Ali Ghorbani Moghadam <amoghada@cisco.com>
2019-10-22 19:36:03 -0400

    using BUILD_TAG in  push.sh

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build --build-arg JAR_FILE=build/libs/auth-svc.jar -t $DOCKER_REGISTRY/auth-svc:$BUILD_NUMBER.$BUILD_ID .
+docker build --build-arg JAR_FILE=build/libs/auth-svc.jar -t $DOCKER_REGISTRY/auth-svc:$BUILD_TAG .
 echo $DOCKER_PSWRD | docker login --username $DOCKER_USER --password-stdin $DOCKER_REGISTRY
-docker push $DOCKER_REGISTRY/auth-svc:$BUILD_NUMBER.$BUILD_ID
+docker push $DOCKER_REGISTRY/auth-svc:$BUILD_TAG
