4fa6ac8e20188d0710c7e30c12b685ed4d352f96
amoghada <amoghada@cisco.com>
2019-11-13 08:58:54 -0600

    update push script

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build --build-arg JAR_FILE=build/libs/auth-svc.jar -t $DOCKER_REGISTRY/auth-svc:$BUILD_ID .
-cat $GOOGLE_KEYFILE | docker login -u _json_key --password-stdin https://$DOCKER_REGISTRY
+echo $GOOGLE_KEYFILE | docker login -u _json_key --password-stdin https://$DOCKER_REGISTRY
 docker push $DOCKER_REGISTRY/auth-svc:$BUILD_ID
